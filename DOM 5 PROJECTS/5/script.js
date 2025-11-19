719068d58fd02d76479bf7ad907f431885b039a5
ajay gour <ajaugour6264@gmail.com>
2025-11-19 15:37:37 +0530

    insta like features on story

--- DOM 5 PROJECTS/5/script.js
+++ DOM 5 PROJECTS/5/script.js
@@ -1,1 +1,47 @@
 
+
+var arr  = [
+    {
+       dp:"https://i.pinimg.com/736x/25/06/87/250687c2ae6377d2de816c47c619a6bc.jpg",
+       story:"https://i.pinimg.com/736x/be/13/24/be13246b81de4d28a1b912b971c826d4.jpg"
+    },
+    {
+       dp:"https://i.pinimg.com/736x/ca/d7/8e/cad78eb43c7cb7a4e6f1c4fa392d8fa4.jpg",
+       story:"https://i.pinimg.com/736x/03/55/12/035512e8c56652ed35837ed6485e8cdc.jpg"
+    },
+    {
+       dp:"https://i.pinimg.com/736x/25/06/87/250687c2ae6377d2de816c47c619a6bc.jpg",
+       story:"https://i.pinimg.com/736x/be/13/24/be13246b81de4d28a1b912b971c826d4.jpg"
+    },
+    {
+       dp:"https://i.pinimg.com/736x/25/06/87/250687c2ae6377d2de816c47c619a6bc.jpg",
+       story:"https://i.pinimg.com/736x/be/13/24/be13246b81de4d28a1b912b971c826d4.jpg"
+    },
+    {
+       dp:"https://i.pinimg.com/736x/25/06/87/250687c2ae6377d2de816c47c619a6bc.jpg",
+       story:"https://i.pinimg.com/736x/be/13/24/be13246b81de4d28a1b912b971c826d4.jpg"
+    },
+]
+
+var clutter = "";
+arr.forEach(function(elem , idx){
+  clutter += `  <div  class="story">
+                <img id="${idx}" src="${elem.dp}" alt="">
+            </div>`
+ })
+
+ var storiyan = document.querySelector('#storiyan');
+ storiyan.innerHTML = clutter;
+ storiyan.addEventListener('click', function(dets){
+    
+    document.querySelector('#full-screen').style.display = 'block';
+    document.querySelector('#full-screen').style.backgroundImage = `url(${arr[dets.target.id].story})`
+ });
+
+ setTimeout(function(){
+ document.querySelector('#full-screen').style.display = 'none';
+ },4000);
+
+
+ 
+    
